0b2ab548b1a006c6df29e94b34876f014637ba37
Isaiah-Sama <zeldaplayergl@gmail.com>
2023-04-10 18:14:02 -0400

    Setup the firebase core

--- src/firebase.js
+++ src/firebase.js
@@ -1,10 +1,20 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import firebase from "firebase";
+
 const firebaseConfig = {
-    apiKey: "AIzaSyBVuVkulZkhP6ZGtOetg9gNq3HDm2bSN8M",
-    authDomain: "clone-dbc67.firebaseapp.com",
-    projectId: "clone-dbc67",
-    storageBucket: "clone-dbc67.appspot.com",
-    messagingSenderId: "99094345308",
-    appId: "1:99094345308:web:a346be22f0dcb35ab59899",
-    measurementId: "G-PNT229ZYXX"
-  };
+  apiKey: "AIzaSyBVuVkulZkhP6ZGtOetg9gNq3HDm2bSN8M",
+  authDomain: "clone-dbc67.firebaseapp.com",
+  projectId: "clone-dbc67",
+  storageBucket: "clone-dbc67.appspot.com",
+  messagingSenderId: "99094345308",
+  appId: "1:99094345308:web:a346be22f0dcb35ab59899",
+  measurementId: "G-PNT229ZYXX",
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+// the database
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { db, auth };
