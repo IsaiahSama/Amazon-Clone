df56d9589c592627880fff416f47b177d7cf5e86
Isaiah-Sama <zeldaplayergl@gmail.com>
2023-04-10 11:58:34 -0400

    Setup core for reducer

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { StateProvider } from "./StateProvider";
+import { initialState } from "./reducer";
+import reducer from "./reducer";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
