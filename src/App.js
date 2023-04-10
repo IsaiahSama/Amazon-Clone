bc4b0e3095828773814e5566ce5b8b52e2593967
Isaiah-Sama <zeldaplayergl@gmail.com>
2023-04-09 21:35:50 -0400

    Adding Body section

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import "./App.css";
 import Header from "./Header";
+import Home from "./Home";
 
 function App() {
   return (
     <div className="app">
       {/* <h1>NOT an Amazon Clone 🌝</h1> */}
       <Header />
-      {/* Home */}
+      <Home />
     </div>
   );
 }
---
+++ src/Home.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Header.css";
+
+function Home() {
+  return <div></div>;
+}
+
+export default Home;
