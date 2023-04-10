dc3e1a65eac818d5916afc4221dd1ce60dd4ec7c
Isaiah-Sama <zeldaplayergl@gmail.com>
2023-04-09 22:57:00 -0400

    Added routes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import "./App.css";
 import Header from "./Header";
 import Home from "./Home";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="app">
-      {/* <h1>NOT an Amazon Clone 🌝</h1> */}
-      <Header />
-      <Home />
-    </div>
+    // BEM
+    <Router>
+      <div className="app">
+        <Routes>
+          <Route
+            path="/checkout"
+            element={[<Header />, <h1>I am a checkout page</h1>]}
+          />
+          <Route path="/" element={[<Header />, <Home />]} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
