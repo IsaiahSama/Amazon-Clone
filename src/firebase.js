f7163869f934eda2b2cdb706fa0687d265f1c79e
Isaiah-Sama <zeldaplayergl@gmail.com>
2023-04-09 20:36:11 -0400

    Added firebase.js

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBVuVkulZkhP6ZGtOetg9gNq3HDm2bSN8M",
+    authDomain: "clone-dbc67.firebaseapp.com",
+    projectId: "clone-dbc67",
+    storageBucket: "clone-dbc67.appspot.com",
+    messagingSenderId: "99094345308",
+    appId: "1:99094345308:web:a346be22f0dcb35ab59899",
+    measurementId: "G-PNT229ZYXX"
+  };
