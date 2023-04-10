ade78be0528f519f8c57fb489404ce318fd6ccc1
Isaiah-Sama <zeldaplayergl@gmail.com>
2023-04-09 21:02:26 -0400

    Added content for the header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import './App.css';
+import "./App.css";
+import Header from "./Header";
 
 function App() {
   return (
     <div className="app">
       <h1>NOT an Amazon Clone 🌝</h1>
+      <Header />
+      {/* Home */}
     </div>
   );
 }
