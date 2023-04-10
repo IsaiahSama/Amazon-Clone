28fdfd8ec32b11cddd02103ca318b54f3eac7fbb
Isaiah-Sama <zeldaplayergl@gmail.com>
2023-04-09 21:48:15 -0400

    Added CSS File, and modified the Banner

--- src/Home.js
+++ src/Home.js
@@ -1,8 +1,17 @@
 import React from "react";
-import "./Header.css";
+import "./Home.css";
 
 function Home() {
-  return <div></div>;
+  return (
+    <div className="home">
+      <div className="home__container">
+        <img
+          className="home__image"
+          src="https://m.media-amazon.com/images/I/61mL9ySACvL._SX3000_.jpg"
+        />
+      </div>
+    </div>
+  );
 }
 
 export default Home;
