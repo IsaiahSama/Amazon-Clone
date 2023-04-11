e9785ecaeb80630c9c66b462a13a82b96adda9b1
Isaiah-Sama <zeldaplayergl@gmail.com>
2023-04-11 18:41:44 -0400

    Added payment HTML

--- src/Payment.js
+++ src/Payment.js
@@ -1,7 +1,11 @@
 import React from "react";
 import "./Payment.css";
+import { useStateValue } from "./StateProvider";
+import CheckoutProduct from "./CheckoutProduct";
 
 function Payment() {
+  const [{ basket, user }, dispatch] = useStateValue();
+
   return (
     <div className="payment">
       <div className="payment__container">
@@ -11,11 +15,38 @@ function Payment() {
           <div className="payment__title">
             <h3>Delivery Address</h3>
           </div>
+          <div className="payment__address">
+            <p>{user?.email}</p>
+            <p>123 Behind You Lane</p>
+            <p>Don't Blink, JK</p>
+          </div>
         </div>
         {/* Review Items  */}
-        <div className="payment__section"></div>
+        <div className="payment__section">
+          <div className="payment__title">
+            <h3>Review Items and Delivery</h3>
+          </div>
+          <div className="payment__items">
+            {basket.map((item) => (
+              <CheckoutProduct
+                id={item.id}
+                title={item.title}
+                image={item.image}
+                price={item.price}
+                rating={item.rating}
+              />
+            ))}
+          </div>
+        </div>
         {/* Payment Method  */}
-        <div className="payment__section"></div>
+        <div className="payment__section">
+          <div className="payment__title">
+            <h3>Payment Method</h3>
+          </div>
+          <div className="payment__details">
+            {/* Stripe Stuff goes here :D  */}
+          </div>
+        </div>
       </div>
     </div>
   );
