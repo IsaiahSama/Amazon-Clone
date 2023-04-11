c7ac6a11f8d7449d74aa35d3c1283a193e9d65d4
Isaiah-Sama <zeldaplayergl@gmail.com>
2023-04-11 18:25:50 -0400

    Setup pages for payment

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from "./Header";
 import Home from "./Home";
 import Checkout from "./Checkout";
 import Login from "./Login";
+import Payment from "./Payment";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { UserAuthContextProvider, useStateValue } from "./StateProvider";
 import React, { useEffect } from "react";
@@ -41,6 +42,7 @@ function App() {
           <Routes>
             <Route path="/checkout" element={[<Header />, <Checkout />]} />
             <Route path="/login" element={[<Login />]} />
+            <Route path="/payment" element={[<Header />, <Payment />]} />
             <Route path="/" element={[<Header />, <Home />]} />
           </Routes>
         </UserAuthContextProvider>
