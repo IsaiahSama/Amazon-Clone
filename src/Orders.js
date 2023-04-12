214ed57000ad4026ae046181146439e89a9c6239
Isaiah-Sama <zeldaplayergl@gmail.com>
2023-04-11 22:51:53 -0400

    Fixed database issues with orders

--- src/Orders.js
+++ src/Orders.js
@@ -1,7 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Orders.css";
+import {
+  collection,
+  getDoc,
+  onSnapshot,
+  orderBy,
+  query,
+} from "@firebase/firestore";
+import { useStateValue } from "./StateProvider";
+import { db } from "./firebase";
 
 function Orders() {
+  const [{ basket, user }, dispatch] = useStateValue();
+  const [orders, setOrders] = useState([]);
+
+  useEffect(() => {
+    if (user) {
+      const ordersRef = collection(db, "users", user?.uid, "orders");
+      const q = query(ordersRef, orderBy("created", "desc"));
+      onSnapshot(q, (snapshot) => {
+        setOrders(
+          snapshot.docs.map((doc) => ({ id: doc.id, data: doc.data() }))
+        );
+      });
+    } else {
+      setOrders([]);
+    }
+  }, [user]);
+
   return (
     <div className="orders">
       <h1>Your Orders</h1>
