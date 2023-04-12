2c949c8c4e86147b12adbd70dbd2d8670724539f
Isaiah-Sama <zeldaplayergl@gmail.com>
2023-04-11 23:13:00 -0400

    Will no longer show the button on orders page

--- src/CheckoutProduct.js
+++ src/CheckoutProduct.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./CheckoutProduct.css";
 import { useStateValue } from "./StateProvider";
 
-function CheckoutProduct({ id, title, image, price, rating }) {
+function CheckoutProduct({ id, title, image, price, rating, hideButton }) {
   const [{ basket }, dispatch] = useStateValue();
 
   const removeFromBasket = () => {
@@ -31,7 +31,9 @@ function CheckoutProduct({ id, title, image, price, rating }) {
               <p>✨</p>
             ))}
         </div>
-        <button onClick={removeFromBasket}>Remove from basket</button>
+        {!hideButton && (
+          <button onClick={removeFromBasket}>Remove from basket</button>
+        )}
       </div>
     </div>
   );
